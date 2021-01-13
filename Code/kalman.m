a1147fd8f437a636bb3194d365ae15b0d1678269
Pratham-04 <62358338+Pratham-04@users.noreply.github.com>
2021-01-13 18:45:32 +0530

    Delete Code directory

--- Code/kalman.m
+++
@@ -1,9 +0,0 @@
-function [s, e, k] = kalman(rew, sigma, s, e, k, i, t1)
-    k(i,:) = (s(i-1,:) + sigma^2)./(s(i-1,:) + sigma^2);
-    
-    e(i,:) = e(i-1,:);
-    e(i,t1) = e(i,t1) + k(i,t1)*(rew(i) - e(i-1,t1));
-    
-    s(i,:) = s(i-1,:);
-    s(i,t1) = (1 - k(i,t1))*(s(i-1,t1) + sigma^2);
-end
--- Code/make_reward.m
+++
@@ -1,4 +0,0 @@
-function [u1 u2] = make_reward(patchA, patchB)
-    u1 = [patchA/2 patchA patchB patchB/2];
-    u2 = [patchA/2 patchB patchA patchA/2];
-end
